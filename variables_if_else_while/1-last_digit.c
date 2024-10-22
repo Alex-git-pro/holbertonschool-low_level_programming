60882e050e119642a6b778f90d09f7065e3e9f40
Alex-git-pro <alexandre.mousse.git@gmail.com>
2024-10-22 15:22:42 +0700

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -4,6 +4,13 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ *  main - Entry point
+ *
+ *  Description: Print the last digit of the number stored in the variable n
+ *  Return: Always 0 if success.
+ */
+
 int main(void)
 {
 int n;
@@ -20,7 +27,7 @@ else if (lastdigit == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, lastdigit);
 }
-else 
+else
 {
 printf("last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
 }
