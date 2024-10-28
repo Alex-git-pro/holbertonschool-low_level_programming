e3d6ad0c4c6921eaefb4c97574d50dfde40dab5d
Alex-git-pro <alexandre.mousse.git@gmail.com>
2024-10-29 03:02:17 +0700

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draw a diagonal line
+ * @n: number of times the '\' char is printed
+ *Return: is success
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+		_putchar(' ');
+	}
+	_putchar('\\');
+	_putchar(' ');
+	_putchar('\n');
+}
+
+}
+
+
