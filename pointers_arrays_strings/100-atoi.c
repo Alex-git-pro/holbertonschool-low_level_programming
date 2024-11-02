5c199f703d8c54476de9bde34ff8affa628b04b8
Alex-git-pro <alexandre.mousse.git@gmail.com>
2024-11-02 16:32:30 +0700

    100-atoi.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * _atoi - function that convert a string to an integer
+ * @s: parameter
+ * Return: return 0
+ */
+
+int _atoi(char *s)
+{
+	char *ptr = s;
+	int sign = 1;
+	int result = 0;
+
+	while (*ptr != '\0')
+	{
+
+		if (*ptr >= 0 && *ptr <= 9)
+		{
+			result = result * 10 + (*ptr - '0');
+		}
+
+		else if (*ptr == '-')
+		{
+			sign = sign * -1;
+		}
+		ptr++;
+	}
+
+	return (0);
+}
