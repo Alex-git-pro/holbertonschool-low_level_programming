5fc5d3ce8a1604d1c7e9d5e2598ecbde8d210843
Alex-git-pro <alexandre.mousse.git@gmail.com>
2024-11-03 22:28:04 +0700

    6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *cap_string - function that capitalizes all words of a string.e
+ * @s: parameters pointer
+ * Return: return 0
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+	int capitalize_first = 1;
+
+	while (s[i] != '\0')
+	{
+		if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+		{
+			if (capitalize_first && s[i] >= 'a' && s[i] <= 'z')
+			{
+				s[i] = s[i] - 32;
+			}
+			capitalize_first = 1;
+		}
+		i++;
+	}
+	return (s);
+
+}
