937dac3fae18b1f5d4bebdc1f4f29b4d36e7b4e5
Alex-git-pro <alexandre.mousse.git@gmail.com>
2024-11-04 23:05:16 +0700

    1-memcpy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_memcpy - function that copies memory area
+ * @dest: parameters string
+ * @src: parameters
+ * @n: parameters
+ * Return: return 0
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+		for (i = 0; i < n; i++)
+	{
+			dest[i] = src[i];
+	}
+	return (dest);
+}
