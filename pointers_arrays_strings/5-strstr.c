a2d8545f51d27927112cb31bb04ad4c5e536a001
Alex-git-pro <alexandre.mousse.git@gmail.com>
2024-11-06 03:21:31 +0700

    7-print_chessboard

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -13,20 +13,25 @@ char *_strstr(char *haystack, char *needle)
 	int i;
 	int j;
 
+/* Si needle est vide, retourne haystack */
 	if (needle[0] == '\0')
 	{
 		return (haystack);
 	}
 
+/* Parcourt chaque caractère de haystack */
 		for (i = 0; haystack[i] != '\0'; i++)
 		{
+			/* Vérifie si needle apparaît à partir de cette position dans haystack */
 			for (j = 0; needle[j] != '\0'; j++)
 			{
+				/* Si les caractères ne correspondent pas, quitte la boucle */
 				if (haystack[i + j] != needle[j])
 				{
 					break;
 				}
 			}
+			/* Si nous avons parcouru tout needle, une correspondance est trouvée */
 			if (needle[j] == '\0')
 			{
 				return (haystack + i);
