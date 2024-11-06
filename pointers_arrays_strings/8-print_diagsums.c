77e7126df12847e62e8c434b32b49aea5d155c21
Alex-git-pro <alexandre.mousse.git@gmail.com>
2024-11-06 16:57:33 +0700

    8-print_diagsums.c

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -10,14 +10,16 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i;
-	int sum_principal = 0;
-	int sum_secondary = 0;
+	int i;/*Variable pour la boucle*/
+	int sum_principal = 0;/*Somme de la diagonale principale*/
+	int sum_secondary = 0;/* Somme de la diagonale secondaire*/
 
+/*Boucle pour parcourir chaque ligne de la matrice*/
 	for (i = 0; i < size; i++)
 	{
-		sum_principal += a[i * size + i];
-		sum_secondary += a[i * size + (size - 1 - i)];
+		/*Ajouter l'élément de la diagonale principale*/
+		sum_principal += a[i * size + i];/*Accès à la diagonale principale*/
+		sum_secondary += a[i * size + (size - 1 - i)];/*l'élémentdiagonale second*/
 	}
 
 	printf("%d, ", sum_principal);
