751e210c00b8c3c795ca2f316698969df3f294fd
Alex-git-pro <alexandre.mousse.git@gmail.com>
2024-11-09 00:48:23 +0700

    4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,17 +1,36 @@
-#include <stdio>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 /**
- * main - Prints the program name
- * @argc: The number of command line arguments
- * @argv: An array containing the program command line arguments
- * Return: Always 0 (Success)
+ * main - Adds positive numbers.
+ * @argc: Number of arguments.
+ * @argv: Array of argument strings.
+ * Return: 0 if successful, 1 if error occurs.
  */
 
 int main(int argc, char *argv[])
 {
-	(void)argc;
+	int sum = 0;
+	int i, j;
 
-	printf("%s\n", argv[0]);
-
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
 	return (0);
 }
