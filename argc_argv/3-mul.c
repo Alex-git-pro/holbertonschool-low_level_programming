2e2b00da20a6ee8dd610663b55febedab0493b02
Alex-git-pro <alexandre.mousse.git@gmail.com>
2024-11-09 00:27:54 +0700

    3-mul.c

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - write program that multiplies two numbers.
+ * @argc: number.
+ * @argv: array.
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	unsigned int num1;
+	unsigned int num2;
+	unsigned int result;
+	{
+		if (argc != 3)
+		{
+			printf("Error\n");
+			return (1);
+		}
+			num1 = atoi(argv[1]);
+			num2 = atoi(argv[2]);
+			result = num1 * num2;
+		{
+			printf("%d\n", result);
+		}
+		return (0);
+	}
+}
