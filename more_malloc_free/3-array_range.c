cc2e40ddf0426032effb2bc5f9e4adf2e707c88f
Alex-git-pro <alexandre.mousse.git@gmail.com>
2024-11-14 01:40:24 +0700

    3-array_range

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,11 +1,35 @@
-#include "main.h"
 #include <stdlib.h>
 
 /**
- * _calloc - Allocates memory for an array and initializes it to zero
- * @nmemb: Number of elements in the array
- * @size: Size of each element in bytes
+ * array_range - Creates an array of integers from min to max
+ * @min: The minimum value (included)
+ * @max: The maximum value (included)
  *
- * Return: Pointer to the allocated memory, or NULL if it fails
+ * Return: Pointer to the newly created array, or NULL if it fails
  */
+int *array_range(int min, int max)
+{
+	int *arr;
+	int size;
+	int i;
+	int current;
 
+	if (min > max)
+		return (NULL);
+
+	size = max - min + 1;
+
+	arr = malloc(sizeof(int) * size);
+
+	if (arr == NULL)
+		return (NULL);
+
+	current = min;
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = current;
+		current++;
+	}
+
+	return (arr);
+}
