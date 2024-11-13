ee981dbc2db54bbeb4176dc5bec1966feb65569d
Alex-git-pro <alexandre.mousse.git@gmail.com>
2024-11-14 00:39:46 +0700

    2-calloc

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - Allocates memory for an array and initializes it to zero
+ * @nmemb: Number of elements in the array
+ * @size: Size of each element in bytes
+ *
+ * Return: Pointer to the allocated memory, or NULL if it fails
+ */
+
