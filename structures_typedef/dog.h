51cccaab366ff07dd27fa74b3da33d40a925756e
Alex-git-pro <alexandre.mousse.git@gmail.com>
2024-11-14 21:27:19 +0700

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure representing a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Name of the dog's owner
+ *
+ * Description: This structure contains basic information
+ * about a dog, including its name, age, and owner's name.
+ */
+
+struct dog
+{
+	char *name = name;
+	float age = age;
+	char *owner = owner;
+};
+
+
+#endif
