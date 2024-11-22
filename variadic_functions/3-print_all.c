6d823829685c12cb2e99b2ffc2713288fca71b10
Alex-git-pro <alexandre.mousse.git@gmail.com>
2024-11-23 01:12:32 +0700

    3-print_all.c pushed

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,20 +3,17 @@
 #include "variadic_functions.h"
 
 /**
- * print_strings - function that prints strings
- * @separator: string to be printed between numbers
- * @n: number of integers passed to the function
- *
+ * print_all - function that prints anything
+ * @format: list of types of arguments passed to the function
  */
 
 void print_all(const char * const format, ...)
 {
-	unsigned int i;
-	unsigned int j;
+	unsigned int i, j;
 	char *k;
 	va_list vali;
-
 	i = 0;
+
 	while (format != NULL)
 	{
 		va_start(vali, format);
@@ -28,15 +25,12 @@ void print_all(const char * const format, ...)
 				case 'c':
 				printf("%c", va_arg(vali, int));
 				break;
-
 				case 'i':
 				printf("%d", va_arg(vali, int));
 				break;
-
 				case 'f':
 				printf("%f", va_arg(vali, double));
 				break;
-
 				case 's':
 				k = va_arg(vali, char *);
 				if (k == 0)
@@ -47,17 +41,12 @@ void print_all(const char * const format, ...)
 				j = 0;
 				break;
 			}
-
 			if (format[i + 1] && j)
 				printf(", ");
 			i++;
 		}
 		va_end(vali);
 		break;
-		
 	}
 	printf("\n");
-
-
-
 }
