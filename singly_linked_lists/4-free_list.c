cad0e784713058226bc6ef2614e23a17bf537cba
Alex-git-pro <alexandre.mousse.git@gmail.com>
2024-12-03 17:55:36 +0700

    push task 4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_list - function that frees a list_t list.
+ * @head: pointer to pointer to the  head of the list
+ * @str: string
+ */
+
+void free_list(list_t *head)
+
+{
+	list_t *current; /**variable temporaire*/
+	list_t *next;
+
+	current = head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+}
