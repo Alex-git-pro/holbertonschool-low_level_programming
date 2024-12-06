70efbe141b8a5e23dcb48227cac5e9efdf16ae2e
Alex-git-pro <alexandre.mousse.git@gmail.com>
2024-12-07 00:08:09 +0700

    push task 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,10 +1,33 @@
 #include "main.h"
 
 /**
- * read_textfile - function that read Requiescat
+ * append_text_to_file - appends text at the end of a file.
  * @filename: pointer to the filename
- * @letters: the number of letters print
+ * @text_content: a pointer to a string to write to the file
  * Return: the actual number of letters it could read and print
  */
 
-int append_text_to_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content)
+
+{
+	int o, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+		len++;
+	}
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+	return (-1);
+
+	close(o);
+
+	return (1);
+}
