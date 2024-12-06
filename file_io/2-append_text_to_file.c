75e0740320537a2b9c7e4479078632f6a77c7181
Alex-git-pro <alexandre.mousse.git@gmail.com>
2024-12-06 23:07:54 +0700

    push task 1

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that read Requiescat
+ * @filename: pointer to the filename
+ * @letters: the number of letters print
+ * Return: the actual number of letters it could read and print
+ */
+
+int append_text_to_file(const char *filename, char *text_content);
